4bf34089424fc96b6e415fde3c8eef77e5616081
wkvikum <wkvikum@gmail.com>
2026-07-24 12:01:30 +0530

    Add Shared file group with Result view and Score Badges

---
+++ PlayHubApp/Shared/ResultView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultView.swift
+//  PlayHubApp
+//
+//  Created by W.K.W.D.M on 2026-07-24.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultView()
+}
---
+++ PlayHubApp/Shared/ScoreBadge.swift
@@ -0,0 +1,18 @@
+//
+//  ScoreBadge.swift
+//  PlayHubApp
+//
+//  Created by W.K.W.D.M on 2026-07-24.
+//
+
+import SwiftUI
+
+struct ScoreBadge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScoreBadge()
+}
